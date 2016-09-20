5ea3d5632f376b1c9b1ba02561bd4273a66c192b
Devedse <davydavidse@gmail.com>
2016-09-20 16:15:29 +0200

    Logging

--- Scripts/set-version.ps1
+++ Scripts/set-version.ps1
@@ -16,7 +16,9 @@ $SolutionRoot = Split-Path -Path $ScriptDir -Parent
  $ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\DeveMazeGenerator\project.json"
  $re = [regex]"(?<=`"version`":\s`")[.\w-]*(?=`",)"
  
+ Write-Host "ProjectJson Path: $ProjectJsonPath"
  Write-Host "Writing version: $ReleaseVersionNumber$PreReleaseName"
+ Write-Host "Regex: $re"
  
  $re.Replace([string]::Join("`n", (Get-Content -Path $ProjectJsonPath)), "$ReleaseVersionNumber$PreReleaseName", 1) |
  	Set-Content -Path $ProjectJsonPath -Encoding UTF8
