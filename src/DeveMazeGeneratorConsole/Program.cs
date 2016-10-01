5afb34de650a6d704942baa9e5c29ac8f0c1394c
Devedse <davydavidse@gmail.com>
2016-10-01 14:12:52 +0200

    Now also saving the maze

--- src/DeveMazeGeneratorConsole/Program.cs
+++ src/DeveMazeGeneratorConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using DeveMazeGenerator.Generators;
 using DeveMazeGenerator.InnerMaps;
+using ImageProcessorCore;
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace DeveMazeGeneratorConsole
 {
@@ -12,19 +14,51 @@ namespace DeveMazeGeneratorConsole
             var map = new BitArreintjeFastInnerMap(16384, 16384);
             var alg = new AlgorithmBacktrack();
 
-            
+
 
             var w = Stopwatch.StartNew();
             alg.Generate(map, null);
             var elapsed = w.Elapsed;
 
+
+
             Console.WriteLine($"Done in: {elapsed}");
 
-
+            using (var fs = new FileStream("output.png", FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+            {
+                MazeToImage(map, fs);
+            }
             //Console.WriteLine(map.GenerateMapAsString());
 
+            Console.WriteLine("Written file");
 
             Console.ReadKey();
+        }
+
+        public static void MazeToImage(InnerMap map, Stream stream)
+        {
+            var image = new Image(map.Width, map.Height);
+            using (var pixels = image.Lock())
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    for (int x = 0; x < map.Width; x++)
+                    {
+                        var wall = map[x, y];
+
+                        if (!wall)
+                        {
+                            pixels[x, y] = Color.Black;
+                        }
+                        else
+                        {
+                            pixels[x, y] = Color.White;
+                        }
+                    }
+                }
+            }
+
+            image.SaveAsPng(stream);
         }
     }
 }
