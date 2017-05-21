522f74713f2de21e815b8e75f05b8f8b457e6a9f
Devedse <davydavidse@gmail.com>
2017-05-21 14:36:04 +0200

    Fixed the versioning script

--- Scripts/set-version.ps1
+++ Scripts/set-version.ps1
@@ -1,3 +1,8 @@
+$env:APPVEYOR_PULL_REQUEST_NUMBER = ''
+$env:APPVEYOR_BUILD_VERSION = '1.0.145'
+$env:APPVEYOR_REPO_BRANCH = 'CI'
+
+
 $ReleaseVersionNumber = $env:APPVEYOR_BUILD_VERSION
 $PreReleaseName = ''
 
@@ -13,13 +18,15 @@ $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 $ScriptDir = Split-Path -Path $PSScriptFilePath -Parent
 $SolutionRoot = Split-Path -Path $ScriptDir -Parent
 
- $ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\DeveMazeGenerator\Properties\AssemblyInfo.cs"
- #$re = [regex]"(?<=`"version`":\s`")[.\w-\*]*(?=`",)"
- $re = [regex]"(?<=\[assembly: AssemblyVersion\(`")1.0.0.0(?=`")"
+$ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\DeveMazeGenerator\Properties\AssemblyInfo.cs"
+#$re = [regex]"(?<=`"version`":\s`")[.\w-\*]*(?=`",)"
+$re1 = [regex]"(?<=\[assembly: AssemblyVersion\(`")1.0.0.0(?=`")"
+$re2 = [regex]"(?<=\[assembly: AssemblyFileVersion\(`")1.0.0.0(?=`")"
+
+Write-Host "ProjectJson Path: $ProjectJsonPath"
+Write-Host "Writing version: $ReleaseVersionNumber$PreReleaseName"
+Write-Host "Regex 1: $re1"
+Write-Host "Regex 2: $re2"
  
- Write-Host "ProjectJson Path: $ProjectJsonPath"
- Write-Host "Writing version: $ReleaseVersionNumber$PreReleaseName"
- Write-Host "Regex: $re"
- 
- $re.Replace([string]::Join("`n", (Get-Content -Path $ProjectJsonPath)), "$ReleaseVersionNumber$PreReleaseName", 1) |
- 	Set-Content -Path $ProjectJsonPath -Encoding UTF8
+$re1.Replace([string]::Join("`n", (Get-Content -Path $ProjectJsonPath)), "$ReleaseVersionNumber$PreReleaseName", 1) | Set-Content -Path $ProjectJsonPath -Encoding UTF8
+$re2.Replace([string]::Join("`n", (Get-Content -Path $ProjectJsonPath)), "$ReleaseVersionNumber$PreReleaseName", 1) | Set-Content -Path $ProjectJsonPath -Encoding UTF8
