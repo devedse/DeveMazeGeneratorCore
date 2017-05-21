3b7be482714a32d4aef67c4bdb012ca1a4d493ba
Devedse <davydavidse@gmail.com>
2017-05-21 14:49:00 +0200

    Another try for fixing the version

--- src/DeveMazeGenerator/Properties/AssemblyInfo.cs
+++ src/DeveMazeGenerator/Properties/AssemblyInfo.cs
@@ -14,16 +14,16 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.145")]
+//[assembly: AssemblyFileVersion("1.0.145")]
+//[assembly: AssemblyInformationalVersion("1.0.0.0")]
