e6e4f5e271e2f63c34b3a999ae6f31e2c810cd5f
Devedse <davydavidse@gmail.com>
2017-05-21 13:45:27 +0200

    This might fix some build things

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -1,8 +1,8 @@
 call "%~dp0Cleanup.cmd"
 
-rem xcopy "%~dp0..\src\DeveMazeGenerator\bin\Release\netstandard1.0" "%~dp0Output\" /e /y
+rem xcopy "%~dp0..\src\DeveMazeGenerator\bin\Release\netstandard1.1" "%~dp0Output\" /e /y
 
-robocopy "%~dp0..\src\DeveMazeGenerator\bin\Release\netstandard1.0" "%~dp0Output" /E /xf *.pdb
+robocopy "%~dp0..\src\DeveMazeGenerator\bin\Release\netstandard1.1" "%~dp0Output" /E /xf *.pdb
 
 "%~dp0\7z_x64_1602\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveMazeGeneratorCore.zip" "%~dp0Output\*"
 "%~dp0\7z_x64_1602\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveMazeGeneratorCore.7z" "%~dp0Output\*"
