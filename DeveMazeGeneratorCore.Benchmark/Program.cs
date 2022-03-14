5c165689c612810aa521513315175347361d0ac9
Devedse <davydavidse@gmail.com>
2022-03-14 01:20:31 +0100

    Added benchmark

---
+++ DeveMazeGeneratorCore.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DeveMazeGeneratorCore.Benchmark
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Running the Benchmark job");
+            var summary = BenchmarkRunner.Run<MazeBenchmarkJob>();
+        }
+    }
+}
