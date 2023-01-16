6d1b8c4721c59b5e5d32070e03b3af8685c531a1
Devedse <davydavidse@gmail.com>
2023-01-16 02:28:40 +0100

    small checkin

--- Scripts/set-version.ps1
+++ Scripts/set-version.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $ReleaseVersionNumber = $env:APPVEYOR_BUILD_VERSION
 $PreReleaseName = ''
 
-If($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
+If ($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null) {
   $PreReleaseName = '-PR-' + $env:APPVEYOR_PULL_REQUEST_NUMBER
 } ElseIf($env:APPVEYOR_REPO_BRANCH -ne 'master' -and -not $env:APPVEYOR_REPO_BRANCH.StartsWith('release')) {
   $PreReleaseName = '-' + $env:APPVEYOR_REPO_BRANCH
